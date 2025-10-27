552df6c36345d04af755e74d33cbc06bf71751b5
ibrahim-ahmed201 <ibrahimahmed384888@gmail.com>
2025-10-27 15:44:13 +0300

    Fetching data Using async function direct in server component with next and displaying list of cabins

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,13 @@
 import CabinCard from "@app/_components/CabinCard";
+import { getCabins } from "../_library/data-service";
 
 export const metadata = {
   title: "Cabins",
 };
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
-
+  const cabins = await getCabins();
+  console.log(cabins);
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium animate-pulse">
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "qkfipiszaklwcwnlnruv.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/Cabins/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
